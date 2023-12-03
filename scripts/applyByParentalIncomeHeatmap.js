f6812d8cd4c0ff1e42afb0a2c27059339566bb47
alexcjwei <alexcj.wei@gmail.com>
2023-12-03 17:04:28 -0500

    make weight/unweight more efficient

--- scripts/applyByParentalIncomeHeatmap.js
+++ scripts/applyByParentalIncomeHeatmap.js
@@ -12,29 +12,30 @@ d3.csv('data/CollegeAdmissions_Data.csv', (d) => {
     updateVisualization(this.checked, false);
   });
 
+  // Get the unique groups
+  const x = Array.from(new Set(data.map((x) => x.par_income_lab)));
+  const y = Array.from(new Set(data.map((x) => x.tier_name)));
+
+  const groupedData = data.reduce((acc, curr) => {
+    const key = `${curr.tier_name}-${curr.par_income_lab}`;
+    if (!acc[key]) {
+      acc[key] = { sum: 0, weighted_sum: 0, count: 0 };
+    }
+
+    if (!!curr.rel_apply) {
+      // Use weighted values
+      acc[key].weighted_sum += curr.rel_apply;
+    }
+    if (!!curr.rel_apply_unwgt) {
+      // Use unweighted values
+      acc[key].sum += curr.rel_apply_unwgt;
+    }
+
+    acc[key].count += 1;
+    return acc;
+  }, {});
+
   const updateVisualization = (weighted, create) => {
-    // Get the unique groups
-    const x = Array.from(new Set(data.map((x) => x.par_income_lab)));
-    const y = Array.from(new Set(data.map((x) => x.tier_name)));
-
-    const groupedData = data.reduce((acc, curr) => {
-      const key = `${curr.tier_name}-${curr.par_income_lab}`;
-      if (!acc[key]) {
-        acc[key] = { sum: 0, count: 0 };
-      }
-
-      if (weighted && !!curr.rel_apply) {
-        // Use weighted values
-        acc[key].sum += curr.rel_apply;
-      } else if (!weighted && !!curr.rel_apply_unwgt) {
-        // Use unweighted values
-        acc[key].sum += curr.rel_apply_unwgt;
-      }
-
-      acc[key].count += 1;
-      return acc;
-    }, {});
-
     // Initialize the N x M matrix "z" with zeros
     const z = Array.from({ length: x.length }, () =>
       Array.from({ length: y.length }, () => 0)
@@ -45,13 +46,15 @@ d3.csv('data/CollegeAdmissions_Data.csv', (d) => {
       x.forEach((income, j) => {
         const key = `${tier}-${income}`;
         if (groupedData[key]) {
-          z[i][j] = groupedData[key].sum / groupedData[key].count;
+          z[i][j] =
+            (weighted ? groupedData[key].weighted_sum : groupedData[key].sum) /
+            groupedData[key].count;
         }
       });
     });
 
     const layout = {
-      title: 'Relative Applications by Parental Income, by University tier',
+      title: 'Application Rate by Parental Income, by University tier',
       annotations: [],
       xaxis: {
         ticks: '',
