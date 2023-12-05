7924734436cd114dd7e2abdf2d8e7476e733a0be
alexcjwei <alexcj.wei@gmail.com>
2023-12-04 19:23:43 -0500

    adds range slider to toggle btwn charts

---
+++ scripts/setChartResolution.js
@@ -0,0 +1,34 @@
+// Hide div based on resolution value
+const prefixes = [
+  'par_income_lab_vs_attend',
+  'par_income_lab_vs_rel_apply_unwgt_num',
+  'par_income_lab_vs_rel_att_cond_app',
+  'par_income_lab_vs_rel_attend',
+];
+const suffixes = [
+  'high_10_percentile_bucket',
+  'high_5_percentile_bucket',
+  'high_1_percentile_bucket',
+  'high_0.1_percentile_bucket',
+  '1_percent_buckets',
+  'original_buckets',
+];
+const setChartResolution = (idx) => {
+  prefixes.forEach((prefix) => {
+    suffixes.forEach((suffix, i) => {
+      // Set all to hidden, except suffix with index i
+      const chartId = `${prefix}-${i}`;
+      console.log(chartId);
+      const chart = document.getElementById(chartId);
+      console.log(chartId, i !== idx);
+      chart.hidden = i !== idx;
+    });
+  });
+};
+const rangeSlider = document.getElementById('resolution-range');
+const setSlider = (value) => {
+  rangeSlider.setAttribute('value', value);
+};
+
+// Set each to first first
+setChartResolution(0);
