74e0e919404c996ffdaa690531cdeb607bb98eb3
alexcjwei <alexcj.wei@gmail.com>
2023-12-04 19:28:14 -0500

    fixes chart ids

--- scripts/setChartResolution.js
+++ scripts/setChartResolution.js
@@ -1,7 +1,8 @@
 // Hide div based on resolution value
 const prefixes = [
   'par_income_lab_vs_attend',
-  'par_income_lab_vs_rel_apply_unwgt_num',
+  'par_income_bin_vs_rel_attend',
+  'par_income_lab_vs_rel_apply_unwgt',
   'par_income_lab_vs_rel_att_cond_app',
   'par_income_lab_vs_rel_attend',
 ];
